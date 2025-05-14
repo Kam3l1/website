cd3b2e65abd264862ae7e4948a23d640567d993d
Kam3l1 <163297078+Kam3l1@users.noreply.github.com>
2025-05-14 18:58:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+// Obracanie zdjęcia w prawo płynnie i nieprzerwanie, 
+document.addEventListener('DOMContentLoaded', function() {
+    const rotatingImage = document.getElementById('rotatingImage');
+    let rotation = 0;
+    
+    function updateRotation() {
+        rotation += 0.5; // Możesz dostosować prędkość zmieniając tę wartość
+        rotatingImage.style.transform = `rotate(${rotation}deg)`;
+        requestAnimationFrame(updateRotation);
+    }
+    
+    updateRotation();
+});
