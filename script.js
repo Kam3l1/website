3fa75c984d360524600a772c2c03927223682619
Kam3l1 <163297078+Kam3l1@users.noreply.github.com>
2025-05-14 18:53:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// Obracanie zdjęcia o 360 stopni w prawo
-const rotatingImage = document.getElementById('rotatingImage');
-let angle = 0;
 
-function rotateImage() {
-    angle = (angle + 1) % 360;
-    rotatingImage.style.transform = `rotate(${angle}deg)`;
-    requestAnimationFrame(rotateImage);
-}
-
-// Uruchomienie animacji po załadowaniu strony
-window.onload = function() {
-    rotateImage();
-};
